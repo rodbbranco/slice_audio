dd5387ff67555422b83efaaa8e2bda002e3af616
rodbbranco <rodbbranco@gmail.com>
2025-09-24 15:43:32 +0100

    Implement lopes fund

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,14 @@
 import { useState } from "react";
 import { AudioUploader } from "@/components/AudioUploader";
 import { AudioSplitter } from "@/components/AudioSplitter";
+import {LinkPreview} from "@/components/ui/link-preview";
 
 export default function Home() {
     const [audioFile, setAudioFile] = useState<File | null>(null);
     const [splitDuration, setSplitDuration] = useState<number>(15);
 
     return (
-        <div className="container mx-auto px-4 py-8 max-w-4xl">
+        <div className="container mx-auto px-4 py-8 max-w-4xl flex flex-col h-dvh justify-between">
             <div className="text-center mb-8 mt-24">
                 <h1 className="text-4xl font-bold text-foreground mb-4">
                     SliceAudio
@@ -19,7 +20,7 @@ export default function Home() {
                 </p>
             </div>
 
-            <div className="space-y-8 mt-32">
+            <div className="space-y-8 -mt-32">
                 <AudioUploader
                     onFileSelect={setAudioFile}
                     selectedFile={audioFile}
@@ -32,6 +33,17 @@ export default function Home() {
                         onDurationChange={setSplitDuration}
                     />
                 )}
+            </div>
+            <div
+                className="text-white/70">
+                    This website has been developed with the generous financial support of
+                    <LinkPreview url="/templates"
+                                 isStatic
+                                 className="font-bold text-white/90"
+                                 imageSrc="/lopes.jpg"
+                    > Francisco Lopes</LinkPreview>
+                    , who contributed $100,000 towards the development of this platform.
+
             </div>
         </div>
     );
